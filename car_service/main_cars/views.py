29a4567d38328c980c057589f4f9d4c7d145e22f
eglemot <egle.mot97@gmail.com>
2023-02-27 16:16:16 +0700

    added filter

--- car_service/main_cars/views.py
+++ car_service/main_cars/views.py
@@ -3,6 +3,7 @@ from . import models
 from django.shortcuts import render
 from django.views import generic
 from django.db.models import Q
+from datetime import date
 
 def index(request):
     order_count = models.Order.objects.count()
@@ -35,14 +36,35 @@ class ModelMakeListView(generic.ListView):
     paginate_by = 6
     template_name = 'main_cars/auto_list.html'
 
+    # views.py
+
+class ModelMakeListView(generic.ListView):
+    model = models.ModelMake
+    paginate_by = 6
+    template_name = 'main_cars/auto_list.html'
+
     def get_queryset(self):
-        qs =  super().get_queryset()
+        qs = super().get_queryset()
+        year_from_filter = self.request.GET.get('year_from')
+        year_to_filter = self.request.GET.get('year_to')
+        if year_from_filter and year_to_filter:
+            qs = qs.filter(year__range=(year_from_filter, year_to_filter))
+        elif year_from_filter:
+            qs = qs.filter(year__gte=year_from_filter)
+        elif year_to_filter:
+            qs = qs.filter(year__lte=year_to_filter)
         query = self.request.GET.get('search')
         if query:
             qs = qs.filter(
                 Q(model__istartswith=query) | Q(make__istartswith=query)
             )
         return qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        years = [(year, year) for year in range(1999, 2024)]
+        context['years'] = years
+        return context
 
 class OrderDetailView(generic.DetailView):
     model = models.Order
